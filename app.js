42d1e5e0acc9ca7109098384f25d9557d40aea1a
Vladyslav Markov <starkxv@airXV.local>
2023-12-08 00:07:41 +0200

    12th commit

--- app.js
+++ app.js
@@ -80,3 +80,43 @@ AFRAME.registerComponent("mouse-move", {
         }
     }
 });
+
+AFRAME.registerComponent('fade-in', {
+    schema: {
+        duration: {type: 'number', default: 500},
+    },
+
+    init() {
+        this.el.addEventListener('model-loaded', this.onModelLoaded.bind(this));
+    },
+
+    onModelLoaded() {
+        const mesh = this.el.getObject3D('mesh');
+        if (!mesh) return;
+        this.setOpacity(mesh, 0);
+        this.mesh = mesh;
+        this.startTime = Date.now() + 2000;
+        this.animateFadeIn();
+    },
+
+    animateFadeIn() {
+        if (!this.mesh || !this.startTime) return;
+
+        const elapsedTime = Date.now() - this.startTime;
+        const progress = THREE.MathUtils.clamp(elapsedTime / this.data.duration, 0, 1);
+        this.setOpacity(this.mesh, progress);
+
+        if (progress < 1) {
+        requestAnimationFrame(this.animateFadeIn.bind(this));
+        }
+    },
+
+    setOpacity(mesh, opacity) {
+        mesh.traverse((node) => {
+        if (node.isMesh) {
+            node.material.transparent = true;
+            node.material.opacity = opacity;
+        }
+        });
+    },
+});
